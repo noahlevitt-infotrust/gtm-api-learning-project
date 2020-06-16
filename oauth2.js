b4a761a026c68a045c4f37044fbad61f1de89184
Noah Levitt <noah.levitt@infotrustllc.com>
2020-06-16 19:53:01 -0400

    sample oauth

--- oauth2.js
+++ oauth2.js
@@ -1,84 +1,99 @@
-// Copyright 2012 Google LLC
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//    http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-'use strict';
-
 const fs = require('fs');
-//const path = require('path');
-const http = require('http');
-const url = require('url');
-const opn = require('open');
-const destroyer = require('server-destroy');
+const util = require('util');
+const readline = require('readline');
 
 const {google} = require('googleapis');
-const plus = google.plus('v1');
 
-/**
- * To use OAuth2 authentication, we need access to a a CLIENT_ID, CLIENT_SECRET, AND REDIRECT_URI.  To get these credentials for your application, visit https://console.cloud.google.com/apis/credentials.
- */
-let authKeyContents = JSON.parse(fs.readFileSync('authkey.json')).installed;
-const oauth2Client = new google.auth.OAuth2(
-    authKeyContents.client_id,
-    authKeyContents.client_secret,
-    authKeyContents.redirect_uris[0]
-);
+const readFilePromise = util.promisify(fs.readFile);
+const writeFilePromise = util.promisify(fs.writeFile);
 
-/**
- * This is one of the many ways you can configure googleapis to use authentication credentials.  In this method, we're setting a global reference for all APIs.  Any other API you use here, like google.drive('v3'), will now use this auth client. You can also override the auth client at the service and method call levels.
- */
-google.options({auth: oauth2Client});
+const SCOPES = [
+  'https://www.googleapis.com/auth/tagmanager.edit.containers',
+  'https://www.googleapis.com/auth/tagmanager.manage.accounts',
+  'https://www.googleapis.com/auth/tagmanager.readonly'
+];
+const TOKEN_PATH = 'token.json';
 
-/**
- * Open an http server to accept the oauth callback. In this simple example, the only request to our webserver is to /callback?code=<code>
- */
-async function authenticate(scopes) {
-  return new Promise((resolve, reject) => {
-    // grab the url that will be used for authorization
-    const authorizeUrl = oauth2Client.generateAuthUrl({
-      access_type: 'offline',
-      scope: scopes.join(' '),
-    });
-    const server = http
-      .createServer(async (req, res) => {
-        try {
-          if (req.url.indexOf('/oauth2callback') > -1) {
-            const qs = new url.URL(req.url, 'http://localhost:3000')
-              .searchParams;
-            res.end('Authentication successful! Please return to the console.');
-            server.destroy();
-            const {tokens} = await oauth2Client.getToken(qs.get('code'));
-            oauth2Client.setCredentials(tokens); // eslint-disable-line require-atomic-updates
-            resolve(oauth2Client);
-          }
-        } catch (e) {
-          reject(e);
-        }
-      })
-      .listen(3000, () => {
-        // open the browser to the authorize url to start the workflow
-        opn(authorizeUrl, {wait: false}).then(cp => cp.unref());
-      });
-    destroyer(server);
-  });
+async function main() {
+  let creds;
+  try {
+    creds = await readFilePromise('authkey.json');
+  } catch (err) {
+    return console.error('Error loading client secret file:', err);
+  }
+
+  const oAuth2Client = await authorize(JSON.parse(creds));
+  await listAccounts(oAuth2Client);
 }
 
-async function runSample() {
-  // retrieve user profile
-  const res = await plus.people.get({userId: 'me'});
+async function authorize(credentials) {
+  const {client_id, client_secret, redirect_uris} = credentials.installed;
+  const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
+
+  let token;
+  try {
+    token = await readFilePromise(TOKEN_PATH);
+  } catch (err) {
+    return getNewToken(oAuth2Client);
+  }
+  oAuth2Client.setCredentials(JSON.parse(token));
+  return oAuth2Client;
+}
+
+async function getNewToken(oAuth2Client) {
+  const getTokenPromise = (code) => {
+    return new Promise((resolve, reject) => {
+      oAuth2Client.getToken(code, (err, token) => {
+        if (err) return reject(err);
+        resolve(token);
+      });
+    });
+  };
+
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: SCOPES,
+  });
+  console.log('Authorize this app by visiting this url:', authUrl);
+
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  const questionPromise = (question) => {
+    return new Promise(resolve => {
+      rl.question(question, answer => {
+        resolve(answer);
+      });
+    });
+  };
+
+  const code = await questionPromise("Enter the code from that page here: ");
+  rl.close();
+
+  let token;
+  try {
+    token = await getTokenPromise(code);
+  } catch (err) {
+    return console.error('Error retrieving access token:', err);
+  }
+
+  oAuth2Client.setCredentials(token);
+  try {
+    await writeFilePromise(TOKEN_PATH, JSON.stringify(token));
+  } catch (err) {
+    return console.error('Error writing to', TOKEN_PATH, err);
+  }
+  console.log('Token stored to', TOKEN_PATH);
+
+  return oAuth2Client;
+}
+
+async function listAccounts(oAuth2Client) {
+  const tagmanager = google.tagmanager({version: 'v2', oAuth2Client});
+
+  const res = await tagmanager.accounts.list();
   console.log(res.data);
 }
 
-const scopes = ['https://www.googleapis.com/auth/plus.me'];
-authenticate(scopes)
-  .then(client => runSample(client))
-  .catch(console.error);
+main().catch(err => { console.error(err); });
