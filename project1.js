997c18ceb39d38eee73b9ebf685962fa2de41263
Noah Levitt <noah.levitt@infotrustllc.com>
2020-06-24 14:54:02 -0400

    finished getContainerByPublicId and findOrCreateContainer

--- project1.js
+++ project1.js
@@ -3,11 +3,16 @@ const {google} = require('googleapis');
 let oAuth2Client, tagmanager;
 const oauth2 = require('./oauth2.js');
 
+const msleep = (ms) => new Promise(resolve => {
+	setTimeout(resolve, ms);
+});
+
 async function cloneExampleContainer(targetContainerName, targetAccountId, exampleContainerPublicId) {
 	oAuth2Client = await oauth2.getOAuth2Client();
 	tagmanager = google.tagmanager({version: 'v2', auth: oAuth2Client});
 
-	const exampleContainer = await getExampleContainerByPublicId(exampleContainerPublicId);
+	const testAccountId = "38028818";
+	const exampleContainer = await getContainerByAccountAndPublicId(testAccountId, exampleContainerPublicId);
 	const targetContainer = await findOrCreateContainer(targetContainerName, targetAccountId);
 	await cloneContainerEntities(exampleContainer, targetContainer);
 }
@@ -16,38 +21,53 @@ async function getAccounts() {
 	return (await tagmanager.accounts.list()).data.account;
 }
 
-async function getExampleContainerByPublicId(publicId) {
-	const accountIds = (await getAccounts()).map((acc) => acc.accountId);
-	let containers = [];
-
-	for (let id in accountIds) {
-		let path = 'accounts/' + id + '/containers/' + publicId;
-		exponentialBackoff(() => {
-			containers.push(tagmanager.accounts.containers.get({path: path}));
-		});
+async function getContainerByPublicId(publicId) {
+	const accountIds = (await getAccounts()).map(acc => acc.accountId);
+	for (let i = 0; i < accountIds.length; i++) {
+		const container = await getContainerByAccountAndPublicId(accountIds[i], publicId);
+		if (container)
+			return container;
+		const ms = 7000;
+		await msleep(ms);
 	}
+	throw "Could not find container.";
+}
+
+async function getContainerByAccountAndPublicId(accountId, publicId) {
+	const path = 'accounts/' + accountId;
+	const response = await tagmanager.accounts.containers.list({parent: path});
+	const containers = response.data.container;
+
+	return !containers ? undefined : containers.find(container => container.publicId === publicId);
+}
+
+async function getContainerByAccountAndName(accountId, targetName) {
+	const path = 'accounts/' + accountId;
+	const response = await tagmanager.accounts.containers.list({parent: path});
+	const containers = response.data.container;
+
+	return !containers ? undefined : containers.find(container => container.name === targetName);
 }
 
 async function findOrCreateContainer(newContainerName, targetAccountId) {
+	const existingContainer = await getContainerByAccountAndName(targetAccountId, newContainerName);
 
+	if (existingContainer)
+		return existingContainer;
+
+	const path = 'accounts/' + targetAccountId;
+	const response = await tagmanager.accounts.containers.create({
+		parent: path,
+		requestBody: {
+			name: newContainerName,
+			usageContext: ['web']
+		}
+	});
+	return response.data;
 }
 
 async function cloneContainerEntities(exampleContainer, targetContainer) {
 
-}
-
-function exponentialBackoff(request) {
-	const msleep = (n) => {
-		Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, n);
-	};
-
-	for (let i = 0; i < 5; i++) {
-		try {
-			request();
-		} catch(err) {
-			msleep(2**i + (Math.random() * 1000));
-		}
-	}
 }
 
 cloneExampleContainer("noah api onboarding container", "28458393", "GTM-PNJH2T")
